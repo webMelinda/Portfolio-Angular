ac8d33e2bb9f6483ebff254ed04f1cc851b0dcbb
webMelinda <melindapinto@gmail.com>
2023-02-16 23:03:01 -0300

    cambios en navbar y diseño responsive

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { About } from 'src/app/model/about';
 import { Red } from 'src/app/model/red';
 import { AboutService } from 'src/app/servicios/about.service';
-import { DatosService } from 'src/app/servicios/datos.service';
 import { RedService } from 'src/app/servicios/red.service';
 
 
